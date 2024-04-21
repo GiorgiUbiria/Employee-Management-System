478298e5b23b8f4cb19c0f9d3752f50682cb7eac
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-21 22:32:41 +0400

    Add - SignInAsync implementation

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -1,6 +1,7 @@
 using BaseClass.DTOs;
 using BaseClass.Entities;
 using BaseClass.Responses;
+using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic;
@@ -52,7 +53,24 @@ public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext app
 
     public async Task<LoginResponse> SignInAsync(Login user)
     {
-        throw new NotImplementedException();
+        if (user is null) return new LoginResponse(false, "Model is empty.");
+
+        var applicationUser = await FindUserByEmail(user.Email!);
+        if (applicationUser is null) return new LoginResponse(false, "User not found.");
+
+        if (!BCrypt.Net.BCrypt.Verify(user.Password!, applicationUser.Password))
+            return new LoginResponse(false, "Email or Password are not valid.");
+
+        var getUserRole = await appDbContext.UserRoles.FirstOrDefaultAsync(_ => _.UserId == applicationUser.Id);
+        if (getUserRole is null) return new LoginResponse(false, "User's role not found.");
+
+        var getRoleName = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Id == getUserRole.RoleId);
+        if (getRoleName is null) return new LoginResponse(false, "User's role not found.");
+
+        string jwtToken = GenerateToken(applicationUser, getRoleName!.Name!);
+        string refreshToken = GenerateRefreshToken();
+
+        return new LoginResponse(true, "Login successfully!", jwtToken, refreshToken);
     }
 
     private async Task<ApplicationUser> FindUserByEmail(string email) =>
