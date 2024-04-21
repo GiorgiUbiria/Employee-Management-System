41ee1639f69cba43dbc465b8e508a3cb88a1726b
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-21 22:19:14 +0400

    Add - AddRole Migration

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e053525a5a18fbc9c1c14f2d17afe62ed2f4fba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a272491d515dc7af64a17d668473a05525e4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
