82e12fd3d16e5f224da4dd60903095b023110f69
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-05-08 17:30:57 +0400

    Add - Images directory. Styling for authentication layout

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c19bc1779cf4f6ecf1beaeb027d84b89fe7dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b292c8cdf55f089eac558749885f97d45a89e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c19bc1779cf4f6ecf1beaeb027d84b89fe7dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b292c8cdf55f089eac558749885f97d45a89e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
